0771944bc09ff0ec829e7773dbd942e390eaf7e8
NIKITA KHANDAL <78340078+NIKITA0509-I@users.noreply.github.com>
2022-07-29 08:41:55 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import pickle
 
 
 app = Flask(__name__)
-model = pickle.load(open('houseprice(mr).pkl','rb')) 
-
+model = pickle.load(open('houseprediction.pkl','rb')) 
 
 @app.route('/')
 def home():
@@ -17,17 +16,24 @@ def home():
 def predict():
     
     
-    '''
-    For rendering results on HTML GUIss
-    '''
-    exp = float(request.args.get('exp'))
     
-    prediction = model.predict([[exp]])
+    '''
+    For rendering results on HTML GUI
+    '''
+    home = float(request.args.get('home'))
+    SqFt = float(request.args.get('SqFt'))
+    bed = float(request.args.get('bed'))
+    bath = float(request.args.get('bath'))
+    offer = float(request.args.get('offers'))
+    brick = float(request.args.get('brick'))
+    neighborhood = float(request.args.get('neighborhood'))
+    
+
+    prediction = model.predict([[home,SqFt,bed,bath,offer,brick,neighborhood]])
     
         
-    return render_template('index.html', prediction_text='Regression Model  has predicted salary for given experinace is : {}'.format(prediction))
-
-if _name_ == '_main_':
-      app.run(host='0.0.0.0', port=80)
+    return render_template('index.html', prediction_text='Regression Model  has predicted house price : {}'.format(prediction))
 
 
+if __name__ == "__main__":
+    app.run()
